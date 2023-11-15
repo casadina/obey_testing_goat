12ed30b2b028e176aa4eb6ddef1a9e8e7edcd696
casadina <83548095+casadina@users.noreply.github.com>
2023-11-15 14:33:00 +0800

    Add basic _get_latest_source() to clone or pull commit.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -19,15 +19,40 @@ def deploy(ctx):
         port=2222,
         connect_kwargs={"key_filename": getenv('SSH_PKEY')}
     ) as c:
-        c.run(f"mkdir -p /home/{USERNAME}/test_getenv")
-        c.run(f"ls -al /home/goat")
-        print("Created test directory?")
-        """
-        c.run(f"cd /home/USERNAME/sites/{SITENAME} && git pull")
-        c.run(f"cd /home/USERNAME/sites/your_site && /home/USERNAME/.virtualenvs/your_virtualenv/bin/pip install -r requirements.txt")
-        c.run(f"cd /home/USERNAME/sites/your_site && /home/USERNAME/.virtualenvs/your_virtualenv/bin/python manage.py migrate --noinput")
-        c.run(f"cd /home/USERNAME/sites/your_site && /home/USERNAME/.virtualenvs/your_virtualenv/bin/python manage.py collectstatic --noinput")
-        """
+        _get_latest_source(c)
+        # _update_virtualenv(c)
+        # _create_or_update_dotenv(c)
+        # _update_static_files(c)
+        #_update_database(c)
+
+
+def _get_latest_source(c):
+    """Change to the directory where the source code is located and pull the latest code from the repository."""
+    if c.run(f"test -d /home/{USERNAME}/sites/{SITENAME}/.git", warn=True).failed:
+        c.run(f"git clone {REPO_URL} /home/{USERNAME}/sites/{SITENAME}")
+    c.run(f"cd /home/{USERNAME}/sites/{SITENAME} && git pull")
+
+
+def _update_virtualenv(c):
+    """Change to the directory where the source code is located and update the virtual environment."""
+    c.run(f"cd /home/{USERNAME}/sites/{SITENAME}"
+          f"&& /home/{USERNAME}/sites/{SITENAME}.venv/bin/pip install -r requirements.txt")
+
+
+def _create_or_update_dotenv(c):
+    """Create or update the .env file in the source code directory."""
+    current_contents = c.run(f"cat /home/{USERNAME}/sites/{SITENAME}/.env", hide=True).stdout
+    if "DJANGO_DEBUG_FALSE=y" not in current_contents:
+        current_contents = current_contents.append("DJANGO_DEBUG_FALSE=y")
+    if f"SITENAME={SITENAME}" not in current_contents:
+        current_contents = current_contents.append(f"SITENAME={SITENAME}")
+    if "DJANGO_SECRET_KEY" not in current_contents:
+        new_secret = "".join(random.SystemRandom().choices("abcdefghijklmnopqrstuvwxyz0123456789", k=50))
+        current_contents = current_contents.append(f"DJANGO_SECRET_KEY={new_secret}")
+    # TODO: Change to .env if all works well and the .env test file looks correct
+    c.run(f"echo \"{current_contents}\" > /home/{USERNAME}/sites/{SITENAME}/TEST_RUN_DELETE")
+
+
 
 
 ctx = Context()
