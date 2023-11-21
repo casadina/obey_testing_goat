2de71598b4c5d1d33fe15b08236668bd68b8357f
casadina <83548095+casadina@users.noreply.github.com>
2023-11-21 15:13:51 +0800

    Add function to create or update the .env file.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,5 +1,7 @@
 import random
 import subprocess
+from os.path import exists
+
 from dotenv import load_dotenv
 from fabric import Connection, task
 from invoke import Context
@@ -24,7 +26,7 @@ def deploy(ctx):
     ) as c:
         _get_latest_source(c)
         _update_virtualenv(c)
-        # _create_or_update_dotenv(c)
+        _create_or_update_dotenv(c)
         # _update_static_files(c)
         # _update_database(c)
 
@@ -35,20 +37,20 @@ def _get_latest_source(c):
         c.run(f"{GIT_PATH} clone {REPO_URL} {CD_PATH}")
     current_commit = subprocess.check_output([GIT_PATH, "log", "-n 1", "--format=%H"]).decode("utf-8").strip("\n")
     c.run(f"cd {CD_PATH} && {GIT_PATH} fetch")
-    # TODO: Fabric doesn't like the reset command. It runs but receives this error:
-    # fatal: not a git repository (or any of the parent directories): .git
     c.run(f'cd {CD_PATH} && {GIT_PATH} reset --hard {current_commit}')
     c.run(f'cd {CD_PATH} && {GIT_PATH} log -n 1 --format=%H')
 
 
 def _update_virtualenv(c):
     """Change to the directory where the source code is located and update the virtual environment."""
+    if not exists(f"{CD_PATH}/.venv/bin/pip"):
+        c.run(f"python3 -m venv {CD_PATH}/.venv")
     c.run(f"{CD_PATH}/.venv/bin/pip install -r {CD_PATH}/requirements.txt")
 
 
 def _create_or_update_dotenv(c):
     """Create or update the .env file in the source code directory."""
-    current_contents = c.run(f"cat /home/{USERNAME}/sites/{SITENAME}/.env", hide=True).stdout
+    current_contents = c.run(f"cat {CD_PATH}/.env", hide=True).stdout
     if "DJANGO_DEBUG_FALSE=y" not in current_contents:
         current_contents = current_contents.append("DJANGO_DEBUG_FALSE=y")
     if f"SITENAME={SITENAME}" not in current_contents:
@@ -56,9 +58,8 @@ def _create_or_update_dotenv(c):
     if "DJANGO_SECRET_KEY" not in current_contents:
         new_secret = "".join(random.SystemRandom().choices("abcdefghijklmnopqrstuvwxyz0123456789", k=50))
         current_contents = current_contents.append(f"DJANGO_SECRET_KEY={new_secret}")
-    # TODO: Change to .env if all works well and the .env test file looks correct
-    c.run(f"echo \"{current_contents}\" > /home/{USERNAME}/sites/{SITENAME}/TEST_RUN_DELETE")
-    c.run(f"cat /home/{USERNAME}/sites/{SITENAME}/TEST_RUN_DELETE")
+    c.run(f"echo \"{current_contents}\" > {CD_PATH}/.env")
+    c.run(f"cat {CD_PATH}/.env")
 
 
 context = Context()
