64797855a6c12837c0d3fea20d8967c8961577de
casadina <83548095+casadina@users.noreply.github.com>
2023-11-21 15:27:29 +0800

    Check if redundant load_dotenv is needed in settings file. Leave deleted if not necessary.

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
-
-load_dotenv('/Users/mimi/coding/obey_testing_goat/.env')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
