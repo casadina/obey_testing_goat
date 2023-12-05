b95f87a4aef2da02c7d6807beadf73bb38a479c5
casadina <83548095+casadina@users.noreply.github.com>
2023-12-05 15:34:39 +0800

    rename all item input ids and names. still broken

--- lists/views.py
+++ lists/views.py
@@ -15,19 +15,21 @@ def view_list(request, list_id):
 
     if request.method == "POST":
         try:
-            item = Item(text=request.POST["item_text"], list=our_list)
+            item = Item(text=request.POST["text"], list=our_list)
             item.full_clean()
             item.save()
             return redirect(f'/lists/{our_list.id}/')
         except ValidationError:
+            our_list.delete()
             error = "You can't have an empty list item"
+            return render(request, 'home.html', {"error": error})
 
     return render(request, "list.html", {"list": our_list, "error": error})
 
 
 def new_list(request):
     nulist = List.objects.create()
-    item = Item.objects.create(text=request.POST["item_text"], list=nulist)
+    item = Item.objects.create(text=request.POST["text"], list=nulist)
     try:
         item.full_clean()
         item.save()
