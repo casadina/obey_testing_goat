1b326db4813f6bb7cf7f91db45513e7a1befef2e
casadina <83548095+casadina@users.noreply.github.com>
2024-01-10 15:43:40 +0800

    Separate click and input functional test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -91,9 +91,14 @@ class ItemValidationTest(FunctionalTest):
             )
         )
 
+    def test_error_messages_are_cleared_on_click(self):
         # She then decides to be clever and sends another
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Banter too thick")
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.get_item_input_box().send_keys("a")
+        self.wait_for_row_in_list_table("1: Banter too thick")
+        self.get_item_input_box().send_keys("Banter too thick")
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for(
             lambda: self.assertTrue(
                 self.get_error_element().is_displayed()
